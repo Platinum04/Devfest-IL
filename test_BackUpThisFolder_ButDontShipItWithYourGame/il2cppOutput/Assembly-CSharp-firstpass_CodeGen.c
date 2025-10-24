1d35b0c5adefd7da75136e04ecd6e7e3a62b8b9e
Awodi Abdulmujeeb <awodi04@gmail.com>
2025-10-24 23:46:43 +0100

    cleanup 5

---
+++ test_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp-firstpass_CodeGen.c
@@ -0,0 +1,63 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mC7CA174A23290C34424DF6D2733D5E64B92E5977 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_m3C9D09F94200334DD5FA29A465481C7848AF4549 (void);
+extern void ButtonClickSound_Awake_mD5BA7669FA865EC03CB95AC040D1EC2BD3F527A7 (void);
+extern void ButtonClickSound_PlayClickSound_m5D9401D7871DC9217BE6F14260A4058005971F1B (void);
+extern void ButtonClickSound__ctor_m3F8B976F09960CAA4463FFECECBD9845CE92C859 (void);
+extern void ButtonClickSound_U3CAwakeU3Eb__2_1_mBD4968ED2A9436F0D22F437E0653FC2756E5C0B1 (void);
+extern void U3CU3Ec__cctor_mBD4CF0AACCF7AF75F56E3E8B2266E057984F687F (void);
+extern void U3CU3Ec__ctor_m0E097CE480E567A148BACFE7EA34CE7C2E27564E (void);
+extern void U3CU3Ec_U3CAwakeU3Eb__2_0_mA4F484E7FF4A5193D5A54BAD4495BA9D7B396471 (void);
+static Il2CppMethodPointer s_methodPointers[9] = 
+{
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mC7CA174A23290C34424DF6D2733D5E64B92E5977,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_m3C9D09F94200334DD5FA29A465481C7848AF4549,
+	ButtonClickSound_Awake_mD5BA7669FA865EC03CB95AC040D1EC2BD3F527A7,
+	ButtonClickSound_PlayClickSound_m5D9401D7871DC9217BE6F14260A4058005971F1B,
+	ButtonClickSound__ctor_m3F8B976F09960CAA4463FFECECBD9845CE92C859,
+	ButtonClickSound_U3CAwakeU3Eb__2_1_mBD4968ED2A9436F0D22F437E0653FC2756E5C0B1,
+	U3CU3Ec__cctor_mBD4CF0AACCF7AF75F56E3E8B2266E057984F687F,
+	U3CU3Ec__ctor_m0E097CE480E567A148BACFE7EA34CE7C2E27564E,
+	U3CU3Ec_U3CAwakeU3Eb__2_0_mA4F484E7FF4A5193D5A54BAD4495BA9D7B396471,
+};
+static const int32_t s_InvokerIndices[9] = 
+{
+	30455,
+	17695,
+	17695,
+	17695,
+	17695,
+	13159,
+	30408,
+	17695,
+	9199,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharpU2Dfirstpass_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpU2Dfirstpass_CodeGenModule = 
+{
+	"Assembly-CSharp-firstpass.dll",
+	9,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
